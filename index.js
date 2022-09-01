7d1668984a1f58def3ef4e097324e3b83a3a7ef8
Evans <evans.njeru@student.moringaschool.com>
2022-09-01 05:59:25 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+const destinationURL = "http://localhost:3000/users";
+
+function appendToDOM(msg) {
+	const body = document.querySelector("body");
+	const p = document.createElement("p");
+	const txt = document.createTextNode(msg);
+	p.appendChild(txt);
+	body.appendChild(p);
+}
+
+function submitData(username, email) {
+	const userData = {
+		name: username,
+		email: email
+	};
+	const configurationObject = { 
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json"
+		},
+		body: JSON.stringify(userData)
+	};
+	return fetch(destinationURL, configurationObject)
+		.then(response => response.json())
+		.then(data => {
+			appendToDOM(data.id);
+		})
+		.catch(error => {
+			appendToDOM(error.msg);
+		});
+} 
